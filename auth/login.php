48503d53f31ccfc8cbe40071763b7baa98e68ec2
Ryan Kurniawan <ryansg41@gmail.com>
2022-06-20 21:59:45 +0700

    feat: auth account

--- auth/login.php
+++ auth/login.php
@@ -1,68 +1,112 @@
-<!doctype html>
+<?php include('../config/koneksi.php');
+if (isset($_POST['submit'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $query = mysqli_query($conn, "SELECT * FROM user WHERE email = '$email'");
+    if (mysqli_num_rows($query) > 0) {
+        if ($row = mysqli_fetch_array($query, MYSQLI_ASSOC)) {
+            if (password_verify($password, $row['password'])) {
+
+                $_SESSION['peran'] = $row['peran'];
+                $_SESSION['id'] = $row['id'];
+
+                if ($row['peran'] == 'admin') {
+                    header('location: http://localhost/thebar-gizi/dashboard');
+                } else {
+                    header('location: http://localhost/thebar-gizi');
+                }
+            } else {
+                $errors['password'] = $password == '' ? 'Kolom password kosong' : 'Password salah';
+            }
+        } else {
+            echo "Error: " . mysqli_error($conn);
+        }
+    } else {
+        $errors['email'] = $email == '' ? 'Kolom email kosong' : 'Akun tidak ditemukan';
+        $errors['password'] = $password == '' ? 'Kolom password kosong' : '';
+    }
+    mysqli_close($conn);
+}
+?>
+
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
+
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <style>
-        :root {
-            --main-bg: #475569;
-        }
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-        .main-bg {
-            background: var(--main-bg) !important;
-        }
+    <title>SB Admin 2 - Login</title>
 
-        input:focus,
-        button:focus {
-            border: 1px solid var(--main-bg) !important;
-            box-shadow: none !important;
-        }
+    <!-- Custom fonts for this template-->
+    <link href="<?= $base_url; ?>/assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
-        .form-check-input:checked {
-            background-color: var(--main-bg) !important;
-            border-color: var(--main-bg) !important;
-        }
+    <!-- Custom styles for this template-->
+    <link href="<?= $base_url; ?>/assets/css/sb-admin-2.css" rel="stylesheet">
 
-        .card,
-        .btn,
-        input {
-            border-radius: 0 !important;
-        }
-    </style>
 </head>
 
-<body class="main-bg">
+<body class="bg-gradient-primary">
+
     <div class="container">
-        <div class="row justify-content-center mt-5">
-            <div class="col-lg-4 col-md-6 col-sm-6">
-                <div class="card shadow">
-                    <div class="card-title text-center border-bottom">
-                        <h2 class="p-3">Login</h2>
-                    </div>
-                    <div class="card-body">
-                        <form>
-                            <div class="mb-4">
-                                <label for="email" class="form-label">Email</label>
-                                <input type="email" class="form-control" id="email" placeholder="user@gmail.com" />
+
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+
+            <div class="col-xl-10 col-lg-12 col-md-9">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                            <div class="col-lg-6">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                    </div>
+                                    <form class="user" method="POST" action="">
+                                        <div class="form-group">
+                                            <input type="email" name="email" class="form-control form-control-user" aria-describedby="emailHelp" placeholder="Enter Email Address...">
+                                            <small class="text-danger"><?= isset($errors['email']) ? $errors['email'] : ''; ?></small>
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" name="password" class="form-control form-control-user" placeholder="Password">
+                                            <small class="text-danger"><?= isset($errors['password']) ? $errors['password'] : ''; ?></small>
+                                        </div>
+                                        <input type="submit" name="submit" value="Login" class="btn btn-primary btn-user btn-block">
+                                    </form>
+                                    <hr>
+                                    <div class="text-center">
+                                        <a class="small" href="register.php">Create an Account!</a>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="mb-4">
-                                <label for="password" class="form-label">Password</label>
-                                <input type="password" class="form-control" id="password" />
-                            </div>
-                            <div class="d-grid">
-                                <button type="submit" class="btn text-light main-bg">Login</button>
-                            </div>
-                        </form>
+                        </div>
                     </div>
                 </div>
-                <p class="mt-2 text-white">Belum memiliki akun? Silahkan <a class="text-white" href="register.php">daftar</a></p>
+
             </div>
+
         </div>
+
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="<?= $base_url; ?>/assets/vendor/jquery/jquery.min.js"></script>
+    <script src="<?= $base_url; ?>/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="<?= $base_url; ?>/assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="<?= $base_url; ?>/assets/js/sb-admin-2.min.js"></script>
+
 </body>
 
 </html>
--- auth/register.php
+++ auth/register.php
@@ -1,72 +1,122 @@
-<!doctype html>
+<?php include('../config/koneksi.php'); ?>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
+
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <style>
-        :root {
-            --main-bg: #475569;
-        }
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-        .main-bg {
-            background: var(--main-bg) !important;
-        }
+    <title>SB Admin 2 - Register</title>
 
-        input:focus,
-        button:focus {
-            border: 1px solid var(--main-bg) !important;
-            box-shadow: none !important;
-        }
+    <!-- Custom fonts for this template-->
+    <link href="<?= $base_url; ?>/assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
-        .form-check-input:checked {
-            background-color: var(--main-bg) !important;
-            border-color: var(--main-bg) !important;
-        }
+    <!-- Custom styles for this template-->
+    <link href="<?= $base_url; ?>/assets/css/sb-admin-2.css" rel="stylesheet">
 
-        .card,
-        .btn,
-        input {
-            border-radius: 0 !important;
-        }
-    </style>
 </head>
 
-<body class="main-bg">
+<body class="bg-gradient-primary">
+
     <div class="container">
-        <div class="row justify-content-center mt-5">
-            <div class="col-lg-4 col-md-6 col-sm-6">
-                <div class="card shadow">
-                    <div class="card-title text-center border-bottom">
-                        <h2 class="p-3">Login</h2>
-                    </div>
-                    <div class="card-body">
-                        <form>
-                            <div class="mb-4">
-                                <label for="name" class="form-label">Nama Lengkap</label>
-                                <input type="text" class="form-control" id="name" placeholder="John Doe" />
+
+        <div class="card o-hidden border-0 shadow-lg my-5">
+            <div class="card-body p-0">
+                <!-- Nested Row within Card Body -->
+                <div class="row">
+                    <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
+                    <div class="col-lg-7">
+                        <div class="p-5">
+                            <div class="text-center">
+                                <h1 class="h4 text-gray-900 mb-4">Buat Akun</h1>
                             </div>
-                            <div class="mb-4">
-                                <label for="email" class="form-label">Email</label>
-                                <input type="email" class="form-control" id="email" placeholder="user@gmail.com" />
+                            <?php
+                            if (isset($_POST['submit'])) {
+                                $nama = $_POST['nama'];
+                                $email = $_POST['email'];
+                                $password1 = $_POST['password1'];
+                                $password2 = $_POST['password2'];
+                                $alamat = $_POST['alamat'];
+                                $no_hp = $_POST['no_hp'];
+                                $peran = "user";
+                                $errors['nama'] = $nama == '' ? 'Kolom nama kosong' : '';
+                                $errors['email'] = $email == '' ? 'Kolom email kosong' : '';
+                                $errors['alamat'] = $alamat == '' ? 'Kolom alamat kosong' : '';
+                                $errors['no_hp'] = $no_hp == '' ? 'Kolom no hp kosong' : '';
+                                $errors['password1'] = $password1 == '' ? 'Kolom password kosong' : '';
+                                $errors['password2'] = $password2 == '' ? 'Kolom ketik ulang password kosong' : '';
+                                $errors['password'] = $password1 !== $password2 ? "Password tidak sama" : '';
+
+                                $password = password_hash($password1, PASSWORD_DEFAULT);
+                                $sql = "INSERT INTO user VALUES (NULL, '$nama', '$email', '$password', '$alamat', '$no_hp', '$peran')";
+                                if (mysqli_query($conn, $sql)) {
+                                    echo "";
+                                } else {
+                                    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                                }
+                                mysqli_close($conn);
+                            }
+                            ?>
+                            <form class="user" action="" method="POST">
+                                <div class="form-group row">
+                                    <div class="col-sm-12 mb-3 mb-sm-0">
+                                        <input type="text" name="nama" class="form-control form-control-user" placeholder="Nama Lengkap">
+                                        <small class="text-danger"><?= isset($errors['nama']) ? $errors['nama'] : ''; ?></small>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <input type="email" name="email" class="form-control form-control-user" placeholder="Email Address">
+                                    <small class="text-danger"><?= isset($errors['email']) ? $errors['email'] : ''; ?></small>
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-sm-6 mb-3 mb-sm-0">
+                                        <input type="password" name="password1" class="form-control form-control-user" placeholder="Password">
+                                        <small class="text-danger"><?= isset($errors['password']) ? $errors['password'] : ''; ?></small>
+                                        <small class="text-danger"><?= isset($errors['password1']) ? $errors['password1'] : ''; ?></small>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <input type="password" name="password2" class="form-control form-control-user" placeholder="Repeat Password">
+                                        <small class="text-danger"><?= isset($errors['password']) ? $errors['password'] : ''; ?></small>
+                                        <small class="text-danger"><?= isset($errors['password2']) ? $errors['password2'] : ''; ?></small>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <input type="number" name="no_hp" class="form-control form-control-user" placeholder="08xxxx">
+                                    <small class="text-danger"><?= isset($errors['no_hp']) ? $errors['no_hp'] : ''; ?></small>
+                                </div>
+                                <div class="form-group">
+                                    <textarea name="alamat" class="form-control" cols="30" rows="5" placeholder="Alamat"></textarea>
+                                    <small class="text-danger"><?= isset($errors['alamat']) ? $errors['alamat'] : ''; ?></small>
+                                </div>
+                                <input type="submit" name="submit" class="btn btn-primary btn-user btn-block" value="Register Account">
+                            </form>
+                            <hr>
+                            <div class="text-center">
+                                <a class="small" href="login.php">Already have an account? Login!</a>
                             </div>
-                            <div class="mb-4">
-                                <label for="password" class="form-label">Password</label>
-                                <input type="password" class="form-control" id="password" />
-                            </div>
-                            <div class="d-grid">
-                                <button type="submit" class="btn text-light main-bg">Login</button>
-                            </div>
-                        </form>
+                        </div>
                     </div>
                 </div>
-                <p class="mt-2 text-white">Sudah memiliki akun? Silahkan <a class="text-white" href="login.php">login</a></p>
             </div>
         </div>
+
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="<?= $base_url; ?>/assets/vendor/jquery/jquery.min.js"></script>
+    <script src="<?= $base_url; ?>/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="<?= $base_url; ?>/assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="<?= $base_url; ?>/assets/js/sb-admin-2.min.js"></script>
+
 </body>
 
 </html>
