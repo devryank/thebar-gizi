f7a6977e8d25156d622fd200b39d21854282db91
Ryan Kurniawan <ryansg41@gmail.com>
2022-06-26 10:59:58 +0700

    fix: show every order in one invoice

--- keranjang.php
+++ keranjang.php
@@ -1,35 +1,42 @@
 <?php include('template/header.php'); ?>
 <?php
-$result_transaksi = mysqli_query($conn, "SELECT * FROM transaksi");
-$data_transaksi = mysqli_fetch_array($result_transaksi, MYSQLI_ASSOC);
-var_dump($data_transaksi);
-die;
+$result_transaksi = mysqli_query($conn, "SELECT * FROM transaksi WHERE id_user = " . $_SESSION['id'] . " order by tanggal desc");
+$data_transaksi = mysqli_fetch_all($result_transaksi, MYSQLI_ASSOC);
 ?>
 
 <?php
 foreach ($data_transaksi as $key => $data) :
 ?>
-    <div class="card">
+    <div class="card mt-2">
         <div class="card-body">
-            <h4 class="">INV-202205230001</h4>
+            <h4 class=""><?= $data['kode_transaksi'];?></h4>
             <hr>
             <div class="row ms-3">
                 <?php
                 $result_daftar_pembelian = mysqli_query($conn, "SELECT d.*, p.nama, p.foto, p.harga, p.kategori FROM daftar_pembelian d INNER JOIN produk p ON d.kode_produk = p.id WHERE d.kode_transaksi = '" . $data['kode_transaksi'] . "'");
-                $data_pembelian = mysqli_fetch_array($result_daftar_pembelian, MYSQLI_ASSOC);
+                $data_pembelian = mysqli_fetch_all($result_daftar_pembelian, MYSQLI_ASSOC);
+                $total = 0;
+                foreach ($data_pembelian as $data_p) :
+                    $total += $data_p['harga'] * $data_p['jumlah'];
                 ?>
                 <div class="col-lg-2 my-2">
-                    <img src="<?= $base_url; ?>/assets/img/<?php echo $data_pembelian['kategori'] . "/" . $data_pembelian['foto'] ?>" width="100%">
+                    <img src="<?= $base_url; ?>/assets/img/<?php echo $data_p['kategori'] . "/" . $data_p['foto'] ?>" width="100%">
                 </div>
                 <div class="col-lg-10">
-                    <?= $data_pembelian['nama']; ?>
+                    <?= $data_p['nama']; ?>
                     <br>
-                    <?php echo "Rp " . number_format($data_pembelian['harga'], 0, ',', '.') . " x " . $data_pembelian['jumlah'] . " = " . "Rp " . number_format($data_pembelian['harga'] * $data_pembelian['jumlah'], 0, ',', '.') ?>
+                    <?php echo "Rp " . number_format($data_p['harga'], 0, ',', '.') . " x " . $data_p['jumlah'] . " = " . "Rp " . number_format($data_p['harga'] * $data_p['jumlah'], 0, ',', '.') ?>
                 </div>
+                <?php endforeach;?>
             </div>
             <hr>
-            <div class="col-lg-12">
-                <h5 class="text-success fw-bold"><?php echo "Rp " . number_format($data['total'], 0, ',', '.') ?></h5>
+            <div class="row">
+                <div class="col-lg-8">
+                    <h5 class="text-success fw-bold"><?php echo "Rp " . number_format($total, 0, ',', '.') ?></h5>
+                </div>
+                <div class="col-lg-4 text-end">
+                    <h5 class="text-success fw-bold"><?php echo $data['status'] ?></h5>
+                </div>
             </div>
         </div>
     </div>
